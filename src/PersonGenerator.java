f26d7adc7aa1f294eee6565cea54640e21ed9f73
Scott Browning <scottybrowning12345@gmail.com>
2025-01-20 00:42:01 -0500

    productData.txt added. Some other minor changes

--- src/PersonGenerator.java
+++ src/PersonGenerator.java
@@ -38,7 +38,7 @@ import java.util.Scanner;
                     products.add(productRec);
 
 
-                    done = SafeInput.getYNConfirm(in, "Another person (y/n)? ");
+                    done = SafeInput.getYNConfirm(in, "Are you done? (y/n)? ");
 
 
                 } while (!done);
@@ -68,3 +68,8 @@ import java.util.Scanner;
                 }
             }
         }
+//000001, Pipeweed, Long Bottom Leaf, 600.0
+//000002, Lembas, Elven Wayfare Bread, 200.0
+//000003, Wine, Woodland Elf Wine, 400.0
+//000004, Mushrooms, Farmer Took’s Finest, 125.0
+//            000005, Mithril, Enchanted Dwarven Armor, 3000.0
